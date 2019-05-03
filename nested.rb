435f5507e7b05c30a2632b3f240214ef8205f446
Adella <adellakenn@gmail.com>
2019-05-03 15:03:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,7 +86,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto] = :known_for
+     known_for = (:known_for => "Ruby")
+  programmer_hash[:yukihiro_matsumoto] = known_for
   
   
   programmer_hash
